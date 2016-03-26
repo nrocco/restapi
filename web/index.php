36a83d67160704ca9cf71c4a4155be04905376b7
Nico Di Rocco <dirocco.nico@gmail.com>
2016-03-26 17:25:20 +0100

    Add monolog to the stack

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app->register(new SilexRestApi\Providers\ConfigProvider(__DIR__.'/../config.php'));
+$app->register(new Silex\Provider\MonologServiceProvider());
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register($restapi = new SilexRestApi\Providers\RestApiProvider());
 $app->mount('/', $restapi);
